e5386d9bb6aee20ff33e6454bb41711ce8809bea
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-08 16:11:05 -0600

    Add project files.

---
+++ InterfacesAndTestability.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InterfacesAndTestability.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InterfacesAndTestability.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e8dbc86-0868-4ca9-a6ca-cf0b9893ec80")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InterfacesAndTestability.UnitTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace InterfacesAndTestability.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
+
+
+/*
+ Note that each test class as an attribute called Test class and it can have a bunch of methods like
+
+the one you see here which are decorated with test method attributes in C-sharp attributes represent
+
+meta data about your classes and their members.
+
+They don't have any logic there are just markers.
+
+Then another application or another assembly can read this method data and do something about it.
+
+For example later you're going to see that we're going to use Microsoft test runner and that test runner
+
+ */
